cfdb18728d7f1401fc8c451d647a31ea6a580842
Daft <voglauer.daniel@gmail.com>
2026-03-03 19:55:25 +0100

    add uploading to cli

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 import argparse
+from pathlib import Path
 
-def upload(args):
+from catbox import upload
+
+
+def upload_file(args):
     print(f"Uploading {args.files}, encrypt={args.encrypt} compress={args.compress}")
 
-def download(args):
+    for file in args.files:
+        path = Path(file)
+        if path.is_file():
+            link = upload(path.name, path.read_bytes())
+            print(link)
+
+def download_file(args):
     print(f"Downloading {args.file} to {args.output}")
 
 if __name__ == '__main__':
@@ -15,12 +25,12 @@ if __name__ == '__main__':
     upload_parser.add_argument("files", nargs="+", help="Files to upload")
     upload_parser.add_argument("--encrypt", action="store_true", default=True, help="Encrypt files")
     upload_parser.add_argument("--compress", action="store_true", default=True, help="Compress files")
-    upload_parser.set_defaults(func=upload)
+    upload_parser.set_defaults(func=upload_file)
 
     download_parser = subparsers.add_parser("download", help="Download files")
     download_parser.add_argument("file", help="URL of the file to download")
     download_parser.add_argument("--output", default=".", help="Output path")
-    download_parser.set_defaults(func=download)
+    download_parser.set_defaults(func=download_file)
 
     args = parser.parse_args()
     args.func(args)
